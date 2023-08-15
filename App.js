82ef90a73f8c292e2a3f8f8a225f7251dbdb823c
Mohammad <mehmandoost2@gmail.com>
2023-08-15 20:36:17 +0430

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { useState } from "react";
 
 export default function App() {
+  const [text, setText] = useState("welcome to the app")
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{text}</Text>
+      <Button
+        title="Press me"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
       <StatusBar style="auto" />
     </View>
   );
